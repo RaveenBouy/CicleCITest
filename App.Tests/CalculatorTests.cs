6725d1c9a7b8d03093e44bc86912585597f5343e
RavianXReaver <raveenshenal@gmail.com>
2020-01-26 22:22:04 +0530

    Initial Commit

---
+++ App.Tests/CalculatorTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+using App;
+
+namespace tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnTheTotalOfValue1AndValue2()
+        {
+            var calc = new Calculator(); 
+            var result = calc.Add(12,23);
+            Assert.That(result, Is.EqualTo(35));
+        }
+    }
+}
---
+++ App/Class/Calculator.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace App
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {   
+            return a+b;
+        }
+
+     /*    public static Substract(int a, int b)
+        {
+            return a - b;
+        } */
+    }
+}
